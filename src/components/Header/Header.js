8ccb9292fda74a95df67b7829e678557d404dea3
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-22 17:15:41 +0100

    Add GameScore function compnent to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,16 @@ export default function Header() {
   return (
     <header>
       <h1>PokeMemory!</h1>
+      <GameScore />
     </header>
+  );
+}
+
+function GameScore() {
+  return (
+    <p>
+      <span className="current-score">Score: 0</span> |{" "}
+      <span className="highest-score">Best: 0</span>
+    </p>
   );
 }
