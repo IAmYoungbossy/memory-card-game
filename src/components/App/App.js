beefb5b8c9b600f16d8e41e488dd56efe437d983
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-22 20:06:57 +0100

    Add card component with design

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
       <Footer />
     </div>
   );
---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import "./Card.css";
+
+export default function Card() {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="image-holder"></div>
+        <div className="name-holder">
+          <p>trevenant</p>
+        </div>
+      </div>
+    </div>
+  );
+}
