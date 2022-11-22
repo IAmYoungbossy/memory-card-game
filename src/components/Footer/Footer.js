77c665698effe23f751057fda28651632d05f34a
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-22 17:56:05 +0100

    Add Footer component to page with CSS styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        Copyright &#169; 2022 IAmYoungbossy{" "}
+        <a href="https://github.com/IAmYoungbossy/memory-card-game">
+          <i className="fa fa-github"></i>
+        </a>
+      </small>
+    </footer>
+  );
+}
