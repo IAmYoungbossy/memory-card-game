8330ccd3b79b947a48eb532d9e5d7c2743d92e59
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-23 08:52:38 +0100

    Create a new function compnent for CardContainer and Card divs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Card from '../Card/Card';
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import CardContainer, { Card } from "../Card/Card";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card />
+      <p>Select each card only once to test your memory</p>
+      <CardContainer>
+        <Card />
+      </CardContainer>
       <Footer />
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,15 @@
 import "./Card.css";
 
-export default function Card() {
+export default function CardContainer({ children }) {
+  return <div className="card-container">{children}</div>;
+}
+
+export function Card() {
   return (
-    <div className="card-container">
-      <div className="card">
-        <div className="image-holder"></div>
-        <div className="name-holder">
-          <p>trevenant</p>
-        </div>
+    <div className="card">
+      <div className="image-holder"></div>
+      <div className="name-holder">
+        <p>trevenant</p>
       </div>
     </div>
   );
