9d485f6c579d734dad8e2b2b9aa6e6c8c903514c
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-25 23:20:38 +0100

    Add game over screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import {
 } from "../cssTransition/cssTransition";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import resetGame from "../resetGame/resetGame";
 import CardContainer, { Card } from "../Card/Card";
 import getAllCardsAttributes from "../pokeApi/pokeApi";
@@ -20,6 +20,8 @@ function App() {
   const [gameLevel, setGameLevel] = useState(1);
   const [imageData, setImageData] = useState(img);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [gameOver, setGameOver] = useState(true);
+  const [update, setUpdate] = useState(true);
 
   const handlerMixCardRandomly = (name) => {
     const { rearrangedCards } = rearrangeCards(imageData, name);
@@ -33,10 +35,18 @@ function App() {
       setScore,
       imageData,
       setCardNum,
+      setGameOver,
       setIsLoaded,
       setImageData,
-      setGameLevel
+      setGameLevel,
+      setUpdate,
+      update
     );
+  };
+
+  const handlerGameOverBtn = () => {
+    document.querySelector(".gameOver").classList.add("remove-card");
+    setTimeout(() => setGameOver(true), 200);
   };
 
   const cards = imageData.map((img) => (
@@ -53,6 +63,7 @@ function App() {
     const allSelected = cards.every((card) => card.status === true);
     if (allSelected && cards.length > 0) {
       setIsLoaded(false);
+      update ? setUpdate(false) : setUpdate(true);
       setCardNum((c) => c + 2);
       setGameLevel((c) => c + 1);
     }
@@ -60,22 +71,33 @@ function App() {
 
   useEffect(() => {
     getAllCardsAttributes(setImg, setIsLoaded, setImageData, cardNum);
-  }, [cardNum]);
+  }, [cardNum, update]);
 
   return (
-    <div className="App">
-      <div className="header-container">
-        <Header
-          score={score}
-          best={best}
-        />
-        <p>Level: {gameLevel}</p>
-      </div>
-      <p>Select each card only once to test your memory</p>
-      {isLoaded === false && <LoadingScreen />}
-      {isLoaded && <CardContainer>{cards}</CardContainer>}
-      <Footer />
-    </div>
+    <Fragment>
+      {!gameOver && (
+        <div className="gameOver append-card">
+          <h1>Game Over!</h1>
+          <p>Oops! Please try again.</p>
+          <button onClick={handlerGameOverBtn}>Play Again</button>
+        </div>
+      )}
+      {gameOver && (
+        <div className="App">
+          <div className="header-container">
+            <Header
+              score={score}
+              best={best}
+            />
+            <p>Level: {gameLevel}</p>
+          </div>
+          <p>Select each card only once to test your memory</p>
+          {isLoaded === false && <LoadingScreen />}
+          {isLoaded && <CardContainer>{cards}</CardContainer>}
+          <Footer />
+        </div>
+      )}
+    </Fragment>
   );
 }
 
--- src/components/resetGame/resetGame.js
+++ src/components/resetGame/resetGame.js
@@ -6,14 +6,19 @@ export default function resetGame(
   setScore,
   imageData,
   setCardNum,
+  setGameOver,
   setIsLoaded,
   setImageData,
   setGameLevel,
+  setUpdate,
+  update
 ) {
   setScore(score + 1);
   if (score === best) setBest(best + 1);
   if (name.status) {
-    setCardNum(4)
+    update ? setUpdate(false) : setUpdate(true);
+    setGameOver(false);
+    setCardNum(4);
     setScore(0);
     setBest(best);
     setImageData(imageData);
